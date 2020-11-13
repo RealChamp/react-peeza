ef4d505edff06407eee79a02efe942a6494f15ad
Matvey <matvey.w2015@mail.ru>
2020-11-13 20:31:21 +0400

    create PizzaBlock component & create db file with data & make request to db file

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import {Home, Cart} from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
-
-  
+  const [pizzas, setPizzas] = React.useState([])
+  React.useEffect(()=> {
+  fetch('http://localhost:3000/db.json')
+      .then(response => response.json())
+      .then(json => {
+        setPizzas(json)
+      })
+  },[])
   return (
     <div className="wrapper">
       <Header />
       <div className='content'>
-       <Route path='/' component={Home} exact />
+       <Route path='/' render={() => <Home/>} exact />
        <Route path='/cart' component={Cart} exact />
       </div>
     </div>
